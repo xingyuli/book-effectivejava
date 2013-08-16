1691bafa6c4164fec12c0c44eff822a9bdf91209
Liu Xingyu <xingyulliiuu@gmail.com>
2013-08-16 17:57:12 +0800

    Re-organize chapter2.

--- src/main/java/org/swordess/effectivejava/chapter2/UseStaticFactoryMethodInsteadOfConstructor.java
+++ src/main/java/org/swordess/effectivejava/chapter2/UseStaticFactoryMethodInsteadOfConstructor.java
@@ -1,14 +1,14 @@
-package org.swordess.effectivejava.chapter2.static_factory_method;
+package org.swordess.effectivejava.chapter2;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class UseStaticFactoryMethodInsteadOfConstructor {
 
 	public static void main(String[] args) {
 		Map<String, List<String>> m1 = new HashMap<String, List<String>>();
-		Map<String, List<String>> m2 = Main.newHashMap();
+		Map<String, List<String>> m2 = UseStaticFactoryMethodInsteadOfConstructor.newHashMap();
 	}
 	
 	public static <K, V> HashMap<K, V> newHashMap() {
