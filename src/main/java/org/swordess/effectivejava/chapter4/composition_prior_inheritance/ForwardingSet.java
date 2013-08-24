dc545b5734d64ceb19609f4db86c52ad8199c911
Liu Xingyu <xingyulliiuu@gmail.com>
2013-08-24 19:10:44 +0800

    Learn composition prior inheritance.

---
+++ src/main/java/org/swordess/effectivejava/chapter4/composition_prior_inheritance/ForwardingSet.java
@@ -0,0 +1,37 @@
+package org.swordess.effectivejava.chapter4.composition_prior_inheritance;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+// Reusable forwarding class
+public class ForwardingSet<E> implements Set<E> {
+	
+	private final Set<E> s;
+	
+	public ForwardingSet(Set<E> s) { this.s = s; }
+
+	public void clear() 				{ s.clear(); }
+	public boolean contains(Object o)	{ return s.contains(o); }
+	public boolean isEmpty()			{ return s.isEmpty(); }
+	public int size()					{ return s.size(); }
+	public Iterator<E> iterator()		{ return s.iterator(); }
+	public boolean add(E e)				{ return s.add(e); }
+	public boolean remove(Object o)		{ return s.remove(o); }
+	public boolean containsAll(Collection<?> c)
+										{ return s.containsAll(c); }
+	public boolean addAll(Collection<? extends E> c)
+										{ return s.addAll(c); }
+	public boolean removeAll(Collection<?> c)
+										{ return s.removeAll(c); }
+	public boolean retainAll(Collection<?> c)
+										{ return s.retainAll(c); }
+	public Object[] toArray()			{ return s.toArray(); }
+	public <T> T[] toArray(T[] a)		{ return s.toArray(a); }
+	
+	/* Do not forget to forward these methods to the component as well! */
+	@Override public boolean equals(Object obj) { return s.equals(obj); }
+	@Override public int hashCode() { return s.hashCode(); }
+	@Override public String toString() { return s.toString(); }
+	
+}
