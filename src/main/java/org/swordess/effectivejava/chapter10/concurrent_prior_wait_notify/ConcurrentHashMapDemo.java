64d999fee167b230a056a37c178b2ef3ba2cd41f
Liu Xingyu <xingyulliiuu@gmail.com>
2013-10-22 17:22:36 +0800

    java.util.concurrent prior wait and notify

---
+++ src/main/java/org/swordess/effectivejava/chapter10/concurrent_prior_wait_notify/ConcurrentHashMapDemo.java
@@ -0,0 +1,30 @@
+package org.swordess.effectivejava.chapter10.concurrent_prior_wait_notify;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ConcurrentHashMapDemo {
+
+	private static final ConcurrentMap<String, String> map =
+			new ConcurrentHashMap<String, String>();
+	
+	public static String intern(String s) {
+		String result = map.putIfAbsent(s, s);
+		if (result == null) {
+			result = s;
+		}
+		return result;
+	}
+	
+	public static String fastIntern(String s) {
+		String result = map.get(s);
+		if (result == null) {
+			result = map.putIfAbsent(s, s);
+			if (result == null) {
+				result = s;
+			}
+		}
+		return result;
+	}
+	
+}
